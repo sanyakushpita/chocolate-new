f34d3eb805bf8cb0788751de613a4a28bd4f1c9e
Olexandr Kushpita <kushpita.o@gmail.com>
2023-03-22 00:59:15 +0200

    slider

--- src/slider.js
+++ src/slider.js
@@ -1,7 +1,23 @@
-$('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
+$('.responsive').slick({
   dots: true,
-  dotsClass: 'my-dots',
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 2.5,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
