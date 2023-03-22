0ba8722b77b89d02b70289c9a9728cbc8cb0c803
Olexandr Kushpita <kushpita.o@gmail.com>
2023-03-22 12:59:32 +0200

    fix

--- src/scroll.js
+++
@@ -1,6 +0,0 @@
-function scrollToFooter() {
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: 'smooth',
-  });
-}
--- src/video-iframe.js
+++
@@ -1,5 +0,0 @@
-function playVideo() {
-  var video = document.getElementById('myvideo');
-  video.style.display = 'block';
-  video.src = 'https://www.youtube.com/embed/s0EbAUGftug?autoplay=1';
-}
